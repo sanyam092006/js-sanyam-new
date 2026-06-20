0d29278d98c07e4c143f27837202004f5b5495d3
Sanyam Jain <sanyamj2006@gmail.com>
2026-06-20 19:30:39 +0000

    io

--- js_basics/js_01.js
+++ js_basics/js_01.js
@@ -1,9 +1,29 @@
-console.log("sanyam");
-let c="10";
-console.log(typeof c);
-m=Number(c);
-console.log(typeof m);
-const 
+// console.log("sanyam");
+// let c="10";
+// console.log(typeof c);
+// m=Number(c);
+// console.log(typeof m);
+const obj={
+    "name":"sanyam",
+    
+    "class":"san"
+};
+const obj2={
+        "gender":"male"
+    };
+obj3 ={...obj,...obj2};
+
+const fxn= function(ob){
+return ob.gender;
+}
+console.log(fxn(obj3));
+const arr=[obj,obj2];
+console.log(arr[0].name);
+
+
+
+
+
 
 
 
