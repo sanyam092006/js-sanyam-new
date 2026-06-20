dd1d837b0dee414e09aef8b2f07574322d229f95
Sanyam Jain <sanyamj2006@gmail.com>
2026-06-20 19:30:28 +0000

    new

---
+++ js_basics/js_05_/js_05.js
@@ -0,0 +1,37 @@
+let m="43";
+let c=43;
+if(m===c){
+    console.log("hey");
+}
+else{
+    console.log("no");
+    
+}
+let obj={
+    "ke":"ni",
+    "ko":"MI"
+};
+if (obj.ke=='ni') {
+    console.log("hey");
+    
+    
+} else {
+    console.log("no");
+    
+    
+}
+
+// for( i=0; i<5; i++){
+// for(j=0; j<5; j++){
+//     console.log(i*j);
+    
+// }
+// }
+let o="mother";
+console.log(`hey ${o}`);
+let p=function t(){
+    return null;
+}
+let g=p()??5;
+console.log(g);
+
